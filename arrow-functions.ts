111345edecad1846e3135304bc118444b57c4727
Luke Buthman <lbuthman@gmail.com>
2017-12-16 06:25:28 -0600

    [example] Two ways to use lambdas

---
+++ arrow-functions.ts
@@ -0,0 +1,13 @@
+//basic function
+let log = function(message) {
+    console.log(message);
+}
+
+//single line arrow function
+let doLog = (message) => console.log(message);
+
+//multiline arrow function
+let moreLog = (message) => {
+    console.log(message);
+    console.log("Do more");
+}
